a752455b8aee330dd2718e1025bc9070e5edc9b0
Andrea Lazzaretti <test@test.com>
2025-01-11 22:41:55 +0100

    Use a backend

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,15 +5,43 @@ terraform {
       version = ">= 4.34.0"
     }
   }
+  backend "gcs" {
+    bucket  = "apt-octagon-254417-terraform-state"
+    prefix  = "terraform/state-files"
+  }
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "apt-octagon-254417"
 }
 
 provider "google" {
-  project = "apt-octagon-254417"
+  project = var.project_id
   region  = "europe-west8"
 }
 
-resource "google_service_account" "service_account_default" {
-  account_id   = "example-service-account"
+resource "google_storage_bucket" "terraform_state" {
+  name                        = "${var.project_id}-terraform-state"
+  location                    = "europe-west8"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 90
+    }
+  }
+}
+
+resource "google_service_account" "google_sheets_account" {
+  account_id   = "access-google-sheets"
   display_name = "Access to the Google Sheets"
 }
 
@@ -28,41 +56,41 @@ resource "random_id" "default" {
   byte_length = 8
 }
 
-resource "google_storage_bucket" "default" {
+resource "google_storage_bucket" "function_code_storage_bucket" {
   name                        = "${random_id.default.hex}-gcf-source"
   location                    = "EU"
   uniform_bucket_level_access = true
 }
 
-data "archive_file" "default" {
+data "archive_file" "local_archive_function" {
   type        = "zip"
   output_path = "/tmp/function-source.zip"
   source_dir  = "."
 }
-resource "google_storage_bucket_object" "object" {
-  name   = "h.zip"
-  bucket = google_storage_bucket.default.name
-  source = data.archive_file.default.output_path
+resource "google_storage_bucket_object" "function_code" {
+  name   = "function_code.zip"
+  bucket = google_storage_bucket.function_code_storage_bucket.name
+  source = data.archive_file.local_archive_function.output_path
 }
 
 resource "google_cloudfunctions2_function" "my_function" {
   name        = "google-sheets-function"
   location    = "europe-west8"
-  description = "a new function"
+  description = "This is the function that will write to the Google Sheet"
   build_config {
     runtime = "go123"
     entry_point = "HelloHTTP"
     source {
       storage_source {
-        bucket = google_storage_bucket.default.name
-        object = google_storage_bucket_object.object.name
+        bucket = google_storage_bucket.function_code_storage_bucket.name
+        object = google_storage_bucket_object.function_code.name
       }
     }
   }
 
   service_config {
 
-    service_account_email = google_service_account.service_account_default.email
+    service_account_email = google_service_account.google_sheets_account.email
   }
 }
 
