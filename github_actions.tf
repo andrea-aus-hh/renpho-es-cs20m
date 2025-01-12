1def27eb7368ad349255d08bd1bd4703619257ab
Andrea Lazzaretti <test@test.com>
2025-01-12 20:06:43 +0100

    Rename and correct conditions

--- github_actions.tf
+++ github_actions.tf
@@ -22,18 +22,18 @@ resource "google_project_iam_member" "roles" {
   member = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
-resource "google_iam_workload_identity_pool" "github" {
+resource "google_iam_workload_identity_pool" "github_identity_pool" {
   provider                  = google-beta
   project                   = var.project_id
-  workload_identity_pool_id = "github"
-  display_name              = "github"
+  workload_identity_pool_id = "github-actions"
+  display_name              = "github-actions"
   description               = "Identity pool for GitHub Actions"
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
   provider                           = google-beta
   project                            = var.project_id
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_identity_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "github actions provider"
   description                        = "OIDC identity pool provider for execute GitHub Actions"
@@ -47,13 +47,13 @@ resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
 
-  attribute_condition = "attribute.repository == \"${local.github_repository_name}\" && attribute.ref == \"refs/heads/master\" && attribute.workflow == \"terraform.yml\""
+  attribute_condition = "attribute.repository == \"${local.github_repository_name}\" && attribute.ref == \"refs/heads/master\" && attribute.workflow == \"Terraform\""
 }
 
 resource "google_service_account_iam_member" "github_actions" {
   service_account_id = google_service_account.github_actions.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${local.github_repository_name}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_identity_pool.name}/attribute.repository/${local.github_repository_name}"
 }
 
 output "service_account_github_actions_email" {
