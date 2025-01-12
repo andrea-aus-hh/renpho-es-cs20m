14b0c1422d4a1e05be04763a045c2447d833938d
Andrea Lazzaretti <test@test.com>
2025-01-12 22:23:09 +0100

    Add some functionality

--- sheet_updater_function/hello_http.go
+++ sheet_updater_function/hello_http.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"google.golang.org/api/sheets/v4"
@@ -27,5 +28,42 @@ func HelloHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalf("unable to retrieve spreadsheet: %v", err)
 	}
-	fmt.Fprintf(w, "Spreadsheet Title: %s\n", spreadsheet.Properties.Title)
+	dateToFind := time.Date(2025, time.January, 10, 0, 0, 0, 0, time.UTC)
+	for _, sheet := range spreadsheet.Sheets {
+		sheetName := sheet.Properties.Title
+		log.Printf("Sheet Name: %s\n", sheetName)
+
+		readRange := fmt.Sprintf("%s!B:E", sheetName)
+		resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+		if err != nil {
+			log.Printf("Unable to read data from sheet '%s': %v", sheetName, err)
+			continue
+		}
+
+		for _, row := range resp.Values {
+			if len(row) < 5 {
+				continue
+			}
+
+			dateStr, ok := row[0].(string)
+			if !ok {
+				continue
+			}
+
+			dateParsed, err := time.Parse("02/01/2006", dateStr)
+			if err != nil {
+				log.Printf("Skipping invalid date: %v", err)
+				continue
+			}
+
+			if dateParsed.Equal(dateToFind) || dateParsed.After(dateToFind) {
+				// Weight is in column E (index 4)
+				weight, ok := row[3].(string)
+				if ok {
+					fmt.Fprintf(w, "Weight on %v in sheet '%s': %s\n", dateToFind, sheetName, weight)
+					return
+				}
+			}
+		}
+	}
 }
