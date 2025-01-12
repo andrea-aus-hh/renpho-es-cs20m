9f56d78482a3d5b79ffbc24e85bbb86213d5e5d1
Andrea Lazzaretti <test@test.com>
2025-01-12 19:01:08 +0100

    Add github actions infra

---
+++ enabled-apis.tf
@@ -0,0 +1,14 @@
+locals {
+  services = toset([
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sts.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "service" {
+  for_each = local.services
+  project  = var.project_id
+  service  = each.value
+}
