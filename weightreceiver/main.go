3a83b93693e0322f63d9de3cd241a43382cad619
Andrea Lazzaretti <test@test.com>
2025-01-15 21:51:40 +0100

    Use goroutines

--- weightreceiver/main.go
+++ weightreceiver/main.go
@@ -14,9 +14,10 @@ import (
 
 const targetMACAddress = "ED:67:39:0A:C5:C0"
 
+var adapter = bluetooth.DefaultAdapter
 var stabilizationDuration = 3 * time.Second
 
-func onlyWeightData(rawData []byte) float64 {
+func parseWeightData(rawData []byte) float64 {
 	if len(rawData) < 19 {
 		return -1
 	}
@@ -24,17 +25,47 @@ func onlyWeightData(rawData []byte) float64 {
 	return float64(int(weightBytes[1])<<8|int(weightBytes[0])) / 100.0
 }
 
-func main() {
-	adapter := bluetooth.DefaultAdapter
+func processWeights(incomingWeights <-chan float64, finalWeightDetected chan<- float64) {
+	var currentWeight float64 = -1
+	var lastStableTime time.Time
+	isStable := false
+	for rawWeight := range incomingWeights {
+		if currentWeight != -1 && rawWeight == currentWeight {
+			log.Printf("Weight has been stabled on %.2f for %.0f seconds", rawWeight, time.Since(lastStableTime).Seconds())
+			if time.Since(lastStableTime) >= stabilizationDuration && !isStable {
+				isStable = true
+				finalWeightDetected <- rawWeight
+			}
+		} else {
+			currentWeight = rawWeight
+			lastStableTime = time.Now()
+			isStable = false
+		}
+	}
+}
+
+func scanWeights(incomingWeights chan<- float64) {
 	if adapter.Enable() != nil {
 		fmt.Println("Failed to enable Bluetooth adapter")
+		close(incomingWeights)
 		return
 	}
+	log.Println("Scanning...")
+	adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		address := strings.ToUpper(result.Address.String())
+		if address == targetMACAddress {
+			incomingWeights <- parseWeightData(result.ManufacturerData()[0].Data)
+		}
+	})
+	close(incomingWeights)
+	log.Println("Scan stopped!")
+}
 
+func main() {
 	signalChan := make(chan os.Signal, 1)
-	weightDetected := make(chan float64, 1)
+	finalWeightDetected := make(chan float64, 1)
+	incomingWeights := make(chan float64, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
-
 	go func() {
 		<-signalChan
 		adapter.StopScan()
@@ -42,34 +73,11 @@ func main() {
 		os.Exit(0)
 	}()
 
-	var currentWeight float64 = -1
-	var lastStableTime time.Time
-	isStable := false
-
-	go func() {
-		log.Println("Scanning...")
-		adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
-			address := strings.ToUpper(result.Address.String())
-			if address == targetMACAddress {
-				rawWeight := onlyWeightData(result.ManufacturerData()[0].Data)
-
-				if currentWeight != -1 && rawWeight == currentWeight {
-					log.Printf("Weight has been stabled on %.2f for %.0f seconds", rawWeight, time.Since(lastStableTime).Seconds())
-					if time.Since(lastStableTime) >= stabilizationDuration && !isStable {
-						isStable = true
-						weightDetected <- rawWeight
-					}
-				} else {
-					currentWeight = rawWeight
-					lastStableTime = time.Now()
-					isStable = false
-				}
-			}
-		})
-	}()
+	go scanWeights(incomingWeights)
+	go processWeights(incomingWeights, finalWeightDetected)
 
 	select {
-	case stableWeight := <-weightDetected:
+	case stableWeight := <-finalWeightDetected:
 		adapter.StopScan()
 		fmt.Printf("Stable weight detected: %.2fKg\n", stableWeight)
 	}
