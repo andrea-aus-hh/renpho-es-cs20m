d89e00f9d54341032730fa3f3fa6d56f766199f5
Andrea Lazzaretti <test@test.com>
2025-01-25 15:14:36 +0100

    Commit downloading script

--- weightscanner/download_and_run.bash
+++ weightscanner/download_and_run.bash
@@ -2,8 +2,18 @@
 set -e
 
 REPO="andrea-aus-hh/renpho-es-cs20m"
-DEST_DIR="."
+DEST_DIR="/home/andrea/weightscanner/bin"
 TARBALL="weightscanner.tar.gz"
+
+if [ -f "$DEST_DIR/$TARBALL" ]; then
+    echo "Removing existing tarball: $DEST_DIR/$TARBALL"
+    rm "$DEST_DIR/$TARBALL"
+fi
+
+if [ -d "$DEST_DIR" ]; then
+    echo "Removing existing extracted files in: $DEST_DIR"
+    rm -rf "$DEST_DIR/*"
+fi
 
 ASSET_URL=$(curl -s https://api.github.com/repos/$REPO/releases/latest \
   | grep "browser_download_url.*$TARBALL" \
@@ -21,3 +31,5 @@ curl -L -o "$DEST_DIR/$TARBALL" "$ASSET_URL"
 
 echo "Extracting $DEST_DIR/$TARBALL..."
 tar -xzvf "$DEST_DIR/$TARBALL" -C "$DEST_DIR"
+
+exec "$DEST_DIR/weightscanner"
